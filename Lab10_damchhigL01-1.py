6dbc541561bb97ad2015c47c52625b7e41ef54f8
Damchhig Lama <dlama@student.cscc.edu>
2026-03-29 20:53:11 -0400

    Implement file processing in WordAnalyzer class

--- Lab10_damchhigL01-1.py
+++ Lab10_damchhigL01-1.py
@@ -12,3 +12,28 @@ class WordAnalyzer:
     def __init__(self, filepath):
         self._filepath = Path(filepath)
         self._frequencies = {}
+
+    def process_file(self):
+        try:
+            if not self._filepath.exists():
+                raise FileNotFoundError
+
+            with self._filepath.open('r') as file:
+                translator = str.maketrans('', '', string.punctuation)
+
+                for line in file:
+                    line = line.lower()
+                    line = line.translate(translator)
+                    words = line.split()
+
+                    for word in words:
+                        if word in self._frequencies:
+                            self._frequencies[word] += 1
+                        else:
+                            self._frequencies[word] = 1
+
+            return True
+
+        except FileNotFoundError:
+            print("File not found.")
+            return False
