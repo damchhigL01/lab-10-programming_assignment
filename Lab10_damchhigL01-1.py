73bf9e3c4425f46da5d926815bc477382ff75fd8
Damchhig Lama <dlama@student.cscc.edu>
2026-03-29 20:51:35 -0400

    Create  Word Count Analyzer program

---
+++ Lab10_damchhigL01-1.py
@@ -0,0 +1,10 @@
+"""
+Program Name: Word Count Analyzer
+Author: Damchhig Lama
+Purpose: Count word frequency from selected text files using OOP.
+Date: 03/26/2026
+"""
+
+from pathlib import Path
+import string
+
